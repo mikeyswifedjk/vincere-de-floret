323da8b675786fb4305c2a24ea594d4535f32c8c
Mark James <142849183+markjamesgit@users.noreply.github.com>
2025-05-11 00:06:04 +0800

    feat: footer ui

--- php/customer-landing-page.php
+++ php/customer-landing-page.php
@@ -29,7 +29,7 @@ if ($resultSettings->num_rows > 0) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Sunny Bloom</title>
+  <title>Vincere de Floret</title>
 
   <link rel="stylesheet" href="../css/customer-dashboard.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
