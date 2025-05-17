6162ac6e4d128871e9dae5b1f241ea197a5d66de
Maika Ybiza <117846947+mikeyswifedjk@users.noreply.github.com>
2025-05-17 18:30:00 +0800

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$conn = mysqli_connect("localhost:3306", "root", "", "vincere-de-floret");
+$conn = mysqli_connect("localhost:3306", "root", "", "vincere_de_floret");
 ?>
