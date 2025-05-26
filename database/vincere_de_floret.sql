e0207765875d379041d4b35e5fbf8c348294eed9
Maika Ybiza <117846947+mikeyswifedjk@users.noreply.github.com>
2025-05-26 23:03:15 +0800

    refractor:database

--- database/vincere_de_floret.sql
+++ database/vincere_de_floret.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 26, 2025 at 04:16 AM
+-- Generation Time: May 26, 2025 at 05:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -99,13 +99,6 @@ CREATE TABLE `cart` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`id`, `user_id`, `product_id`, `product_name`, `product_image`, `quantity`, `price`, `created_at`, `updated_at`) VALUES
-(351, 77, 213, 'Bundle 3', '682940c8c0505.jpg', 1, '799', '2025-05-26 01:35:37', '2025-05-26 01:35:37');
-
 -- --------------------------------------------------------
 
 --
@@ -123,12 +116,14 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `category`, `product_count`) VALUES
-(70, 'Funeral Flowers', 12),
 (71, 'Combo Bundle', 13),
 (72, 'Fruits Basket', 12),
 (73, 'Hamper Basket', 6),
 (74, 'Money Bouquet', 6),
-(75, 'Plants ', 6);
+(75, 'Plants ', 6),
+(79, 'Pots', 6),
+(80, 'Flowers', 6),
+(81, 'Add-Ons', 8);
 
 -- --------------------------------------------------------
 
@@ -242,22 +237,36 @@ CREATE TABLE `orders` (
   `discount_code` varchar(255) NOT NULL,
   `receiver_name` varchar(255) NOT NULL,
   `receiver_phone` int(11) NOT NULL,
-  `shipping_status` varchar(255) NOT NULL DEFAULT 'Pending'
+  `shipping_status` varchar(255) NOT NULL DEFAULT 'Pending',
+  `gcash_receipt` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_name`, `sender_name`, `sender_phone`, `address`, `payment_method`, `total_amount`, `order_date`, `status`, `custom_letter`, `region_id`, `discount_code`, `receiver_name`, `receiver_phone`, `shipping_status`) VALUES
-(82, 'test1', 'test', '09123456789', 'Testing St.', 'BDO', 16903.00, '2025-05-21 07:34:12', 'Approved', 'letters/letter_1747812833_2357.pdf', 1, 'AQYL3290', 'receiver', 78945632, 'Out for Delivery'),
-(83, 'test1', 'Testing', '09123456789', 'Testing St.', 'GCash', 25005.00, '2025-05-21 07:37:10', 'Under Review\r\n', NULL, 1, 'FTLX6803', 'receiver', 978456321, 'Pending'),
-(84, 'test1', 'test', '09123456789', 'Testing St.', 'COD', 384.00, '2025-05-21 07:38:26', 'Under Review\r\n', NULL, 1, 'AGJN1506', 'receiver', 912345678, 'Pending'),
-(85, 'test1', 'Testing', '09123456789', 'Testing St.', 'COD', 24085.00, '2025-05-21 10:18:59', 'Under Review', '', 1, 'XLOV3271', 'receiver', 912345678, 'Pending'),
-(86, 'test1', 'Testing', '09123456789', 'Testing St.', 'GCash', 17054.00, '2025-05-25 15:49:31', 'Approved\r\n', 'letters/letter_1748188150_1844.pdf', 2, 'AGJN1506', 'receiver', 978945623, 'Processing'),
-(87, 'test1', 'sender', '03123456789', 'Testing St.', 'GCash', 784.00, '2025-05-26 01:31:50', 'Under Review\r\n', NULL, 4, 'WKUZ9376', 'receiver', 987654321, 'Pending'),
-(88, 'test1', 'sender', '09123456789', 'Testing St.', 'BDO', 14334.00, '2025-05-26 01:37:30', 'Under Review\r\n', NULL, 1, 'GHTI5607', 'Receiver', 945621389, 'Pending'),
-(89, 'test1', 'sender', '098745632', 'Testing St. Test', 'COD', 8424.00, '2025-05-26 01:55:51', 'Under Review', NULL, 1, 'WKUZ9376', 'receiver', 912345678, 'Pending');
+INSERT INTO `orders` (`id`, `user_name`, `sender_name`, `sender_phone`, `address`, `payment_method`, `total_amount`, `order_date`, `status`, `custom_letter`, `region_id`, `discount_code`, `receiver_name`, `receiver_phone`, `shipping_status`, `gcash_receipt`) VALUES
+(82, 'test1', 'test', '09123456789', 'Testing St.', 'BDO', 16903.00, '2025-05-21 07:34:12', 'Approved', 'letters/letter_1747812833_2357.pdf', 1, 'AQYL3290', 'receiver', 78945632, 'Out for Delivery', NULL),
+(83, 'test1', 'Testing', '09123456789', 'Testing St.', 'GCash', 25005.00, '2025-05-21 07:37:10', 'Under Review\r\n', NULL, 1, 'FTLX6803', 'receiver', 978456321, 'Pending', NULL),
+(84, 'test1', 'test', '09123456789', 'Testing St.', 'COD', 384.00, '2025-05-21 07:38:26', 'Under Review\r\n', NULL, 1, 'AGJN1506', 'receiver', 912345678, 'Pending', NULL),
+(85, 'test1', 'Testing', '09123456789', 'Testing St.', 'COD', 24085.00, '2025-05-21 10:18:59', 'Under Review', '', 1, 'XLOV3271', 'receiver', 912345678, 'Pending', NULL),
+(86, 'test1', 'Testing', '09123456789', 'Testing St.', 'GCash', 17054.00, '2025-05-25 15:49:31', 'Approved\r\n', 'letters/letter_1748188150_1844.pdf', 2, 'AGJN1506', 'receiver', 978945623, 'Processing', NULL),
+(87, 'test1', 'sender', '03123456789', 'Testing St.', 'GCash', 784.00, '2025-05-26 01:31:50', 'Under Review\r\n', NULL, 4, 'WKUZ9376', 'receiver', 987654321, 'Pending', NULL),
+(88, 'test1', 'sender', '09123456789', 'Testing St.', 'BDO', 14334.00, '2025-05-26 01:37:30', 'Under Review\r\n', NULL, 1, 'GHTI5607', 'Receiver', 945621389, 'Pending', NULL),
+(89, 'test1', 'sender', '098745632', 'Testing St. Test', 'COD', 8424.00, '2025-05-26 01:55:51', 'Under Review', NULL, 1, 'WKUZ9376', 'receiver', 912345678, 'Pending', NULL),
+(90, 'test1', 'Testing T Testing', '09123456789', 'Testing St. Test', 'COD', 884.00, '2025-05-26 03:10:13', 'Under Review', '0', 1, '', 'Testing Receiver', 912345678, 'Pending', NULL),
+(91, 'test1', 'Testing T Testing', '09123456789', 'Testing St. Test', 'COD', 24985.00, '2025-05-26 03:14:53', 'Under Review', NULL, 1, 'AQYL3290', 'Testing Receiver', 987654321, 'Pending', NULL),
+(92, 'test1', 'Testing T Testing', '09123456789', '123 testing', 'COD', 17584.00, '2025-05-26 03:16:05', 'Under Review', '0', 1, '', 'Testing Receiver', 987654321, 'Pending', NULL),
+(93, 'test1', 'Testing T Testing', '09123456789', 'Testing St.', 'COD', 15244.00, '2025-05-26 03:23:42', 'Under Review', 'letters/letter_1748229719_2534.pdf', 2, 'AQYL3290', 'Testing Receiver', 987654321, 'Pending', NULL),
+(94, 'test1', 'Testing T Testing', '09123456789', 'Testing St', 'GCash', 7084.00, '2025-05-26 14:28:47', 'Under Review', NULL, 1, 'AGJN1506', 'Testing', 97845632, 'Pending', NULL),
+(95, 'test1', 'Testing T Testing', '09123456789', 'Testing St. Test', 'GCash', 15479.00, '2025-05-26 14:32:19', 'Under Review', NULL, 3, 'WKUZ9376', 'Testing Receiver', 97845632, 'Pending', NULL),
+(96, 'test1', 'Testing T Testing', '09123456789', 'dawd', 'GCash', 6540.00, '2025-05-26 14:36:12', 'Under Review', NULL, 1, 'GHTI5607', 'cadsc', 987654321, 'Pending', NULL),
+(97, 'test1', 'Testing T Testing', '09123456789', 'cnksan', 'GCash', -830.00, '2025-05-26 14:40:02', 'Under Review', NULL, 2, 'XLOV3271', 'cb a', 5654, 'Pending', NULL),
+(98, 'test1', 'Testing T Testing', '09123456789', 'fndskl', 'GCash', 7494.00, '2025-05-26 14:47:29', 'Under Review', NULL, 6, 'WKUZ9376', 'fsdf', 4566, 'Pending', NULL),
+(99, 'test1', 'Testing T Testing', '09123456789', 'fndskl', 'GCash', -5.00, '2025-05-26 14:47:52', 'Under Review', NULL, 6, 'WKUZ9376', 'fsdf', 4566, 'Pending', NULL),
+(100, 'test1', 'Testing T Testing', '09123456789', ' feas', 'GCash', 5229.00, '2025-05-26 14:49:51', 'Under Review', NULL, 3, 'WKUZ9376', ' wad', 432, 'Pending', NULL),
+(101, 'test1', 'Testing T Testing', '09123456789', 'fes ', 'GCash', 2634.00, '2025-05-26 14:50:57', 'Under Review', NULL, 1, '', ' gs', 342, 'Pending', NULL),
+(102, 'test1', 'Testing T Testing', '09123456789', 'fs ', 'GCash', 5904.00, '2025-05-26 14:53:25', 'Under Review', NULL, 4, 'GHTI5607', 'daw ', 432, 'Pending', '../img/683480651194d_flower9.jpg');
 
 -- --------------------------------------------------------
 
@@ -287,7 +296,19 @@ INSERT INTO `order_items` (`id`, `order_id`, `product_name`, `quantity`, `price`
 (87, 86, 'Money Bouquet 6', 1, 17459.00, 17459.00, '68294a4fc9789.jpg'),
 (88, 87, 'Bundle 3', 1, 799.00, 799.00, '682940c8c0505.jpg'),
 (89, 88, 'Bundle 10', 1, 15249.00, 15249.00, '6829418771d64.jpg'),
-(90, 89, 'Fruit Basket 3', 1, 8459.00, 8459.00, '682941dc1b6b8.jpg');
+(90, 89, 'Fruit Basket 3', 1, 8459.00, 8459.00, '682941dc1b6b8.jpg'),
+(91, 90, 'Bundle 3', 1, 799.00, 799.00, '682940c8c0505.jpg'),
+(92, 91, 'Funeral Package 1', 1, 25000.00, 25000.00, '6829483545106.jpg'),
+(93, 92, 'Hamper Basket 1', 1, 17499.00, 17499.00, '6829495e74844.jpg'),
+(94, 93, 'Bundle 10', 1, 15249.00, 15249.00, '6829418771d64.jpg'),
+(95, 94, 'Bundle 11', 1, 7499.00, 7499.00, '682941990bb4d.jpg'),
+(96, 95, 'Money Bouquet 3', 1, 15499.00, 15499.00, '68294a1a7b9d4.jpg'),
+(97, 96, 'Funeral Package 2', 1, 7455.00, 7455.00, '68294847c113b.jpg'),
+(98, 97, 'Red Tulip', 1, 75.00, 75.00, '68346ad558b37.jpg'),
+(99, 98, 'Hamper Basket 3', 1, 7499.00, 7499.00, '6829498d45264.jpg'),
+(100, 100, 'Bundle 5', 1, 5249.00, 5249.00, '682940f3ad3f4.jpg'),
+(101, 101, 'Casio Watch ', 1, 2549.00, 2549.00, '68346bc81d028.jpeg'),
+(102, 102, 'Fruit Basket 7', 1, 6799.00, 6799.00, '682944fb06b44.jpg');
 
 -- --------------------------------------------------------
 
@@ -393,18 +414,18 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`id`, `name`, `image`, `category`, `qty`, `price`, `category_id`, `status`, `total_sold`, `available_stocks`) VALUES
 (211, 'Bundle 1', '682940a1e9718.jpg', 'Combo Bundle', 10, '3200', 71, 'Available', 0, 10),
 (212, 'Bundle 2', '682940b76ef86.jpg', 'Combo Bundle', 10, '599', 71, 'Available', 0, 10),
-(213, 'Bundle 3', '682940c8c0505.jpg', 'Combo Bundle', 10, '799', 71, 'Available', 1, 9),
+(213, 'Bundle 3', '682940c8c0505.jpg', 'Combo Bundle', 10, '799', 71, 'Available', 3, 7),
 (214, 'Bundle 4', '682940db9704b.jpg', 'Combo Bundle', 10, '699', 71, 'Available', 0, 10),
 (215, 'Bundle 5', '682940f3ad3f4.jpg', 'Combo Bundle', 10, '5249', 71, 'Available', 0, 10),
 (216, 'Bundle 6', '68294116acebe.jpg', 'Combo Bundle', 10, '12499', 71, 'Available', 0, 10),
 (217, 'Bundle 7', '6829412e8706c.jpg', 'Combo Bundle', 10, '7459', 71, 'Available', 0, 10),
 (218, 'Bundle 8', '6829414d29613.jpg', 'Combo Bundle', 10, '15299', 71, 'Available', 0, 10),
 (219, 'Bundle 9', '682941758f2e6.jpg', 'Combo Bundle', 10, '12599', 71, 'Available', 0, 10),
-(220, 'Bundle 10', '6829418771d64.jpg', 'Combo Bundle', 10, '15249', 71, 'Available', 0, 10),
+(220, 'Bundle 10', '6829418771d64.jpg', 'Combo Bundle', 10, '15249', 71, 'Available', 2, 8),
 (221, 'Bundle 11', '682941990bb4d.jpg', 'Combo Bundle', 10, '7499', 71, 'Available', 0, 10),
 (222, 'Fruit Basket 1', '682941b3a1b7c.jpg', 'Fruits Basket', 10, '7499', 72, 'Available', 0, 10),
 (223, 'Fruit Basket 2', '682941c5e3b9a.jpg', 'Fruits Basket', 10, '4599', 72, 'Available', 0, 10),
-(224, 'Fruit Basket 3', '682941dc1b6b8.jpg', 'Fruits Basket', 10, '8459', 72, 'Available', 2, 8),
+(224, 'Fruit Basket 3', '682941dc1b6b8.jpg', 'Fruits Basket', 10, '8459', 72, 'Available', 3, 7),
 (225, 'Fruit Basket 4', '682941efadcd4.jpg', 'Fruits Basket', 10, '12459', 72, 'Available', 0, 10),
 (226, 'Fruit Basket 5', '68294209b1e27.jpg', 'Fruits Basket', 10, '10455', 72, 'Available', 0, 10),
 (227, 'Fruit Basket 6', '682944671c6c8.jpg', 'Fruits Basket', 10, '11299', 72, 'Available', 0, 10),
@@ -414,7 +435,7 @@ INSERT INTO `product` (`id`, `name`, `image`, `category`, `qty`, `price`, `categ
 (231, 'Fruit Basket 10', '68294581da9e8.jpg', 'Fruits Basket', 10, '13499', 72, 'Available', 0, 10),
 (232, 'Fruit Basket 11', '6829459415f16.jpg', 'Fruits Basket', 10, '17999', 72, 'Available', 0, 10),
 (233, 'Fruit Basket 12', '682945b239889.jpg', 'Fruits Basket', 10, '9899', 72, 'Available', 0, 10),
-(234, 'Funeral Package 1', '6829483545106.jpg', 'Funeral Flowers', 10, '25000', 70, 'Available', 0, 10),
+(234, 'Funeral Package 1', '6829483545106.jpg', 'Funeral Flowers', 10, '25000', 70, 'Available', 1, 9),
 (235, 'Funeral Package 2', '68294847c113b.jpg', 'Funeral Flowers', 10, '7455', 70, 'Available', 0, 10),
 (236, 'Funeral Package 3', '6829485a8b68e.jpg', 'Funeral Flowers', 10, '28155', 70, 'Available', 0, 10),
 (237, 'Funeral Package 4', '6829486b9614a.jpg', 'Funeral Flowers', 10, '7999', 70, 'Available', 0, 10),
@@ -426,7 +447,7 @@ INSERT INTO `product` (`id`, `name`, `image`, `category`, `qty`, `price`, `categ
 (243, 'Funeral Package 10', '682948e504368.jpg', 'Funeral Flowers', 10, '7499', 70, 'Available', 0, 10),
 (244, 'Funeral Package 11', '682949105e97d.jpg', 'Funeral Flowers', 10, '12599', 70, 'Available', 0, 10),
 (245, 'Funeral Package 12', '682949234e658.jpg', 'Funeral Flowers', 10, '21999', 70, 'Available', 0, 10),
-(246, 'Hamper Basket 1', '6829495e74844.jpg', 'Hamper Basket', 10, '17499', 73, 'Available', 0, 10),
+(246, 'Hamper Basket 1', '6829495e74844.jpg', 'Hamper Basket', 10, '17499', 73, 'Available', 1, 9),
 (247, 'Hamper Basket 2', '6829497285881.jpg', 'Hamper Basket', 10, '12599', 73, 'Available', 0, 10),
 (248, 'Hamper Basket 3', '6829498d45264.jpg', 'Hamper Basket', 10, '7499', 73, 'Available', 0, 10),
 (249, 'Hamper Basket 4', '6829499d77af9.jpg', 'Hamper Basket', 10, '5499', 73, 'Available', 0, 10),
@@ -436,14 +457,34 @@ INSERT INTO `product` (`id`, `name`, `image`, `category`, `qty`, `price`, `categ
 (253, 'Money Bouquet 2', '682949fb614ff.jpg', 'Money Bouquet', 10, '10000', 74, 'Available', 0, 10),
 (254, 'Money Bouquet 3', '68294a1a7b9d4.jpg', 'Money Bouquet', 10, '15499', 74, 'Available', 0, 10),
 (255, 'Money Bouquet 4', '68294a2daae2b.jpg', 'Money Bouquet', 10, '7999', 74, 'Available', 0, 10),
-(256, 'Money Bouquet 5', '68294a3ca4412.jpg', 'Money Bouquet', 10, '25000', 74, 'Available', 1, 9),
-(257, 'Money Bouquet 6', '68294a4fc9789.jpg', 'Money Bouquet', 10, '17459', 74, 'Available', 0, 10),
+(256, 'Money Bouquet 5', '68294a3ca4412.jpg', 'Money Bouquet', 10, '25000', 74, 'Available', 2, 8),
+(257, 'Money Bouquet 6', '68294a4fc9789.jpg', 'Money Bouquet', 10, '17459', 74, 'Available', 1, 9),
 (258, 'Plant 1', '68294a90a4bd3.jpg', 'Plants ', 10, '4596', 75, 'Available', 0, 10),
 (259, 'Plant 2', '68294a9d7d2ec.jpg', 'Plants ', 10, '7459', 75, 'Available', 0, 10),
 (260, 'Plant 3', '68294aaa15043.jpg', 'Plants ', 10, '4597', 75, 'Available', 0, 10),
 (261, 'Plant 4', '68294ab7e9c72.jpg', 'Plants ', 10, '4599', 75, 'Available', 0, 10),
 (262, 'Plant 5', '68294acc02cc9.jpg', 'Plants ', 10, '10499', 75, 'Available', 0, 10),
-(263, 'Plant 6', '68294ade8fb7d.jpg', 'Plants ', 10, '3459', 75, 'Available', 0, 10);
+(263, 'Plant 6', '68294ade8fb7d.jpg', 'Plants ', 10, '3459', 75, 'Available', 0, 10),
+(264, 'Red Roses', '68346a53e97dc.jpeg', 'Flowers', 10, '99', 80, 'Available', 0, 10),
+(265, 'Blue Roses', '68346a63b6015.jpg', 'Flowers', 10, '99', 80, 'Available', 0, 10),
+(266, 'Blue Tulip', '68346a8e67890.jpg', 'Flowers', 10, '75', 80, 'Available', 0, 10),
+(267, 'Pink Roses', '68346aa6810f9.jpeg', 'Flowers', 10, '99', 80, 'Available', 0, 10),
+(268, 'Red Tulip', '68346ad558b37.jpg', 'Flowers', 10, '75', 80, 'Available', 0, 10),
+(269, 'Yellow Roses', '68346ae2952c3.jpeg', 'Flowers', 10, '99', 80, 'Available', 0, 10),
+(270, 'Pot 1', '68346b445e744.jpg', 'Pots', 10, '150', 79, 'Available', 0, 10),
+(271, 'Pot 2', '68346b5866cc7.jpg', 'Pots', 10, '150', 79, 'Available', 0, 10),
+(272, 'Pot 3', '68346b64c2f61.jpg', 'Pots', 10, '200', 79, 'Available', 0, 10),
+(273, 'Pot 4', '68346b7346fb5.jpg', 'Pots', 10, '99', 79, 'Available', 0, 10),
+(274, 'Pot 5', '68346b884b8da.jpg', 'Pots', 10, '259', 79, 'Available', 0, 10),
+(275, 'Pot 6', '68346b9d68dd0.jpg', 'Pots', 10, '375', 79, 'Available', 0, 10),
+(276, 'Mugs', '68346bb375d6b.jpg', 'Add-Ons', 10, '250', 81, 'Available', 0, 10),
+(277, 'Casio Watch ', '68346bc81d028.jpeg', 'Add-Ons', 10, '2549', 81, 'Available', 0, 10),
+(278, 'Couple Shirt', '68346bd6a3393.jpg', 'Add-Ons', 10, '399', 81, 'Available', 0, 10),
+(279, 'Versace Eros', '68346be5bed69.jpg', 'Add-Ons', 10, '7459', 81, 'Available', 0, 10),
+(280, 'Blueberry Cheesecake', '68346c02d094a.jpg', 'Add-Ons', 10, '750', 81, 'Available', 0, 10),
+(281, 'White Teddy Bear', '68346c1175f0f.jpg', 'Add-Ons', 10, '499', 81, 'Available', 0, 10),
+(282, 'Brown Teddy Bear XL', '68346c22bc703.jpg', 'Add-Ons', 10, '1299', 81, 'Available', 0, 10),
+(283, 'Mango Cheesecake', '68346c39c7c17.jpg', 'Add-Ons', 10, '1599', 81, 'Available', 0, 10);
 
 -- --------------------------------------------------------
 
@@ -502,8 +543,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `verification_code`, `email_verified_at`, `reset_token`, `reset_token_expiration`, `last_attempt`, `blocked`, `attempts`, `contact_number`, `address`, `image_path`, `first_name`, `middle_name`, `last_name`) VALUES
 (77, 'test1', 'test1@gmail.com', '$2y$10$RmBCWTOeijBRXqOF8uKbie3DSYXd3P2z5.nSwyIPKIkf865NhvIAi', 168148, '2025-05-17 22:16:54.000000', 'a32903518b7e7a0565fc0536f009c12e', '2025-05-25 19:35:18', '2025-05-17 14:16:41', 0, 0, '09123456789', 'Testing St.', '../img/68333de6cb087.jpg', 'Testing', 'T', 'Testing'),
-(78, 'test2', 'test2@gmail.com', '$2y$10$RMxabFjN1GQu.F5R6J5l4OliJ.rU/rRebMTJrQ1mv3TbBT275fYWq', 569408, NULL, NULL, NULL, '2025-05-18 01:58:02', 1, 0, '09123456789', 'Testing St.', NULL, 'Testing', 'T', 'Test'),
-(79, 'itsmy', 'ybiza2018@gmail.com', '$2y$10$WpgiPqf7dcXQHloR4TAscONmszHdNkvUGwPK4gXTn39EGhyLpj5ya', 252240, '2025-05-26 00:40:27.000000', '993df0d8b84cffc5847a333f98543b7b', '2025-05-25 19:41:44', '2025-05-25 16:40:13', 0, 0, '09496563656', 'Kalye Katorse', '', 'Maika', 'Ybiza O.', 'Simbulan');
+(78, 'test2', 'test2@gmail.com', '$2y$10$RMxabFjN1GQu.F5R6J5l4OliJ.rU/rRebMTJrQ1mv3TbBT275fYWq', 569408, NULL, NULL, NULL, '2025-05-18 01:58:02', 1, 0, '09123456789', 'Testing St.', NULL, 'Testing', 'T', 'Test');
 
 --
 -- Indexes for dumped tables
@@ -617,13 +657,13 @@ ALTER TABLE `addons`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=353;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=365;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
 
 --
 -- AUTO_INCREMENT for table `design_settings`
@@ -647,13 +687,13 @@ ALTER TABLE `flower`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT for table `pos_orders`
@@ -677,7 +717,7 @@ ALTER TABLE `pots`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=264;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=284;
 
 --
 -- AUTO_INCREMENT for table `shipping`
